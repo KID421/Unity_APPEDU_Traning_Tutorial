763dd8c3e326ed66cfe16874728a30757257d1bc
KID <50666207+KID421@users.noreply.github.com>
2020-07-13 10:33:19 +0800

    WEB

---
+++ 3DGame/Assets/KID/Scripts/Player.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(AudioSource))]
+public class Player : MonoBehaviour
+{
+    [Header("結束畫面")]
+    public GameObject final;
+    [Header("吃寶箱音效")]
+    public AudioClip soundChest;
+
+    private AudioSource aud;
+    private bool dead;
+
+    private void Start()
+    {
+        aud = GetComponent<AudioSource>();
+
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        Dead();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "寶箱")
+        {
+            GetChest(other.gameObject);
+        }
+    }
+
+    private void GetChest(GameObject obj)
+    {
+        aud.PlayOneShot(soundChest, 1.5f);
+        final.SetActive(true);
+        Cursor.visible = true;
+        Destroy(obj, 0.1f);
+    }
+
+    private void Dead()
+    {
+        if (!dead && transform.position.y < -1)
+        {
+            dead = true;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
